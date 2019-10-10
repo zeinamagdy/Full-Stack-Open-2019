1dcaab3d952886ab1fb0ee4e4aacc30a485e8826
zeinamagdy <zainabmagdy8@gmail.com>
2019-10-10 16:23:48 +0200

    step6:Fetching data using axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './Components/Filter';
 import Persons from './Components/Persons';
 import PersonForm from './Components/PersonForm ';
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-  const [result, setResult] = useState(persons);
+  const [persons, setPersons] = useState([]);
+  const [result, setResult] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+        setResult(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+
 
 
   const handelNewName = (event) => {
