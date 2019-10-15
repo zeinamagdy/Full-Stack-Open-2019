cc756f2aac08dd2c3fef4b3108b8a32571305c94
zeinamagdy <zainabmagdy8@gmail.com>
2019-10-16 00:30:12 +0200

    steps 2.15&2.16&2.17&2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Filter from './Components/Filter';
 import Persons from './Components/Persons';
 import PersonForm from './Components/PersonForm ';
-import axios from 'axios'
+import personService from './services/persons';
+
 
 
 const App = () => {
@@ -11,14 +12,12 @@ const App = () => {
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const hook = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data);
-        setResult(response.data)
+    personService.getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+        setResult(initialPersons)
       })
   }
-
   useEffect(hook, [])
 
 
@@ -32,32 +31,54 @@ const App = () => {
   const handelSearch = (event) => {
     let searchKeyword = event.target.value.toLowerCase();
     if (searchKeyword.length > 0) {
-      setResult(persons.filter((person) => person.name.toLowerCase().includes(searchKeyword)));
+      setResult(result.filter((person) => person.name.toLowerCase().includes(searchKeyword)));
     } else {
       setResult(persons);
     }
   }
+  const deletePerson = (person) => {
+    personService.deletePerson(person)
+      .then(response => {
+        setPersons(result.filter((item) => item.id !== person.id))
+        setResult(result.filter((item) => item.id !== person.id))
+      })
 
+
+  }
   const addName = (event) => {
     event.preventDefault();
     if (newName.length === 0 || newNumber.length === 0) {
       return;
     }
-    if (persons.find((person) => person.name === newName) === undefined) {
-      const personObject = {
-        name: newName,
-        number: newNumber
-      }
-      const newlist = persons.concat(personObject)
-      setPersons(newlist);
-      setResult(newlist);
-      setNewName('');
-      setNewNumber('');
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+    if (result.find((person) => person.name === newName) === undefined) {
+      personService.create(personObject)
+        .then(person => {
+          setPersons(persons.concat(person));
+          setResult(result.concat(person));
+          setNewName('');
+          setNewNumber('');
+        })
     } else {
-      alert(`${newName} is already added to phoneBook`);
+      window.confirm(`${newName} is already added to the phonebook, replace old number with new? `) &&
+        updateNumber(personObject)
+
     }
   }
-
+  const updateNumber = personObject => {
+    const oldObject = result.find((item) => item.name === personObject.name);
+    const oldPerson = result.filter((item) => item.name !== personObject.name)
+    personService.update(oldObject.id, personObject)
+      .then(person => {
+        setPersons(oldPerson.concat(person));
+        setResult(oldPerson.concat(person));
+        setNewName('');
+        setNewNumber('');
+      });
+  }
   return (
     <div>
       <h2>Phonebook</h2>
@@ -70,7 +91,7 @@ const App = () => {
         OnNameAdded={addName} />
       <h2>Numbers</h2>
       <ul>
-        <Persons persons={result} />
+        <Persons persons={result} deletePerson={deletePerson} />
       </ul>
     </div>
   )
--- part2/phonebook/src/Components/Persons.js
+++ part2/phonebook/src/Components/Persons.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, deletePerson }) => {
     return persons.map((person) =>
-        <li key={person.name}>
+        <li key={person.id}>
             {person.name} {person.number}
+            <button
+                onClick={() =>
+                    window.confirm(`Delete ${person.name}?`) &&
+                    deletePerson(person)
+                }
+            >
+                Delete
+            </button>
         </li>)
 }
 export default Persons
